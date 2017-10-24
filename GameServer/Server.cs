f3f43b09702bf01ed68f7d281143318cb07f1357
Zwen 1111 <zwen12@msn.com>
2017-10-24 22:28:25 +0200

    Rebuild

---
+++ GameServer/Server.cs
@@ -0,0 +1,223 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GameServer
+{
+    class Server
+    {
+        static string path;
+
+        static void Main(string[] args)
+        {
+            
+            path = Path.Combine(Directory.GetCurrentDirectory(), "leaderbord.json");
+
+            IPAddress localhost;
+
+            bool ipIsOk = IPAddress.TryParse("127.0.0.1", out localhost);
+            if (!ipIsOk) { Console.WriteLine("ip adres kan niet geparsed worden."); Environment.Exit(1); }
+
+            TcpListener listener = new TcpListener(localhost, 80);
+            listener.Start();
+
+            while (true)
+            {
+                Console.WriteLine(@"
+                      ==============================================
+                        Server started at {0}
+                        Waiting for connection
+                      =============================================="
+                , DateTime.Now);
+
+                TcpClient client1 = listener.AcceptTcpClient();
+                SendMessage(client1, new
+                {
+                    id = "waiting"
+                });
+                TcpClient client2 = listener.AcceptTcpClient();
+                SendMessage(client2, new
+                {
+                    id = "waiting"
+                });
+
+                Thread thread = new Thread(() => HandleClientThread(client1, client2));
+                thread.Start();
+            }
+        }
+
+        static void HandleClientThread(object obj1, object obj2)
+        {
+            TcpClient client1 = obj1 as TcpClient;
+            TcpClient client2 = obj2 as TcpClient;
+
+            SendMessage(client1, new
+            {
+                id = "usernameRequest"
+            });
+
+            string username1 = (string)ReadMessage(client1)["username"];
+
+            SendMessage(client2, new
+            {
+                id = "usernameRequest"
+            });
+
+            string username2 = (string)ReadMessage(client2)["username"];
+
+            SendMessage(client1, new
+            {
+                id = "opponentConnected",
+                data = $"{username2} connected"
+            });
+
+            SendMessage(client2, new
+            {
+                id = "opponentConnected",
+                data = $"{username1} connected"
+            });
+
+            bool win1 = false;
+            bool win2 = false;
+
+            SendMessage(client1, new
+            {
+                id = "yourTurn",
+                mark = "X"
+            });
+
+            SendMessage(client2, new
+            {
+                id = "opponentTurn",
+                mark = "O"
+            });
+
+            string winner = "";
+
+            while (!win1 && !win2)
+            {
+                JObject received = ReadMessage(client1);
+                switch ((string)received["id"])
+                {
+                    case ("opponentSet"):
+                        SendMessage(client2, received);
+                        win1 = Boolean.Parse((string)received["data"]["won"]);
+                        winner = username1;
+                        break;
+                    case ("disconnected"):
+                        SendMessage(client2, received);
+                        win1 = true;
+                        winner = username1;
+                        break;
+                }
+
+                if (win1)
+                    break;
+
+                received = ReadMessage(client2);
+                switch ((string)received["id"])
+                {
+                    case ("opponentSet"):
+                        SendMessage(client1, received);
+                        win2 = Boolean.Parse((string)received["data"]["won"]);
+                        winner = username2;
+                        break;
+                    case ("disconnected"):
+                        SendMessage(client1, received);
+                        win2 = true;
+                        winner = username2;
+                        break;
+                }
+            }
+
+            Dictionary<string, int> leaderbord;
+
+            if (File.Exists(path))
+            {
+                leaderbord = (Dictionary<string, int>)((JObject)JsonConvert.DeserializeObject(File.ReadAllText(path))).ToObject(typeof(Dictionary<string, int>));
+
+                if (leaderbord.ContainsKey(winner))
+                {
+                    int value;
+                    leaderbord.TryGetValue(winner, out value);
+                    leaderbord.Remove(winner);
+                    leaderbord.Add(winner, value++);
+                }
+                else
+                {
+                    leaderbord.Add(winner, 1);
+                }
+            }
+            else
+            {
+                leaderbord = new Dictionary<string, int>();
+                leaderbord.Add(winner, 1);
+            }
+
+            File.WriteAllText(path, JsonConvert.SerializeObject(leaderbord));
+
+            if (client1.Connected)
+            {
+                client1.Close();
+            }
+            if (client2.Connected)
+            {
+                client2.Close();
+            }
+        }
+
+        public static JObject ReadMessage(TcpClient client)
+        {
+            StringBuilder message = new StringBuilder();
+            try
+            {
+                NetworkStream stream = client.GetStream();
+                int numberOfBytesRead = 0;
+                byte[] messageBytes = new byte[4];
+                stream.Read(messageBytes, 0, messageBytes.Length);
+                byte[] receiveBuffer = new byte[BitConverter.ToInt32(messageBytes, 0)];
+
+                do
+                {
+                    numberOfBytesRead = stream.Read(receiveBuffer, 0, receiveBuffer.Length);
+
+                    message.AppendFormat("{0}", Encoding.ASCII.GetString(receiveBuffer, 0, numberOfBytesRead));
+
+                }
+                while (message.Length < receiveBuffer.Length);
+                string response = message.ToString();
+                return JObject.Parse(response);
+            }
+            catch(Exception e)
+            {
+                dynamic data = new
+                {
+                    id = "disconnected",
+                    data = new { }
+                };
+                return JsonConvert.DeserializeObject(JsonConvert.SerializeObject(data));
+            }
+        }
+
+        public static void SendMessage(TcpClient client, dynamic message)
+        {
+            NetworkStream stream = client.GetStream();
+            string json = JsonConvert.SerializeObject(message);
+
+            byte[] prefixArray = BitConverter.GetBytes(json.Length);
+            byte[] requestArray = Encoding.Default.GetBytes(json);
+
+            byte[] buffer = new Byte[prefixArray.Length + json.Length];
+            prefixArray.CopyTo(buffer, 0);
+            requestArray.CopyTo(buffer, prefixArray.Length);
+            stream.Write(buffer, 0, buffer.Length);
+        }
+    }
+}
---
+++ TicTacToe/Client.cs
@@ -0,0 +1,172 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    class Client
+    {
+        TcpClient client;
+        TicTacToe form;
+        NetworkStream stream;
+        Boolean done;
+
+        public Client(TicTacToe ticTacToe)
+        {
+            this.form = ticTacToe;
+            client = new TcpClient("127.0.0.1", 80);
+            stream = client.GetStream();
+        }
+
+        public void StartGame()
+        {
+            form.ClearField();
+
+            done = false;
+            while(!done)
+            {
+                JObject response = ReadMessage();
+
+                switch ((string)response["id"])
+                {
+                    case "usernameRequest":
+                        new Thread(() => 
+                        SendMessage(new
+                        {
+                            username = form.username
+                        })
+                        ).Start();
+                        break;
+                    case ("yourTurn"):
+                        new Thread(() => MyTurn(response)).Start();
+                        break;
+                    case ("opponentTurn"):
+                        new Thread(() => OpponentTurn(response)).Start();
+                        break;
+                    case ("opponentSet"):
+                        new Thread(() => OpponentSet(response)).Start();
+                        break;
+                    case ("waiting"):
+                        new Thread(() => form.AddMessageToConsole("Waiting for an opponent")).Start();
+                        break;
+                    case ("opponentConnected"):
+                        new Thread(() => form.AddMessageToConsole((string)response["data"])).Start();
+                        break;
+                    case ("won"):
+                        new Thread(() => Won()).Start();
+                        break;
+                    case ("disconnected"):
+                        new Thread(() => Disconnected()).Start();
+                        break;
+                }
+            }
+
+            form.DisableButtons();
+
+            client.Close();
+        }
+
+        private void Disconnected()
+        {
+            form.AddMessageToConsole("The opponent lost connection");
+            done = true;
+        }
+
+        private void Won()
+        {
+            form.AddMessageToConsole("You won!");
+            done = true;
+        }
+
+        private void OpponentSet(JObject response)
+        {
+            form.SetButton((int)response["data"]["x"], (int)response["data"]["y"], (string)response["data"]["mark"]);
+            done = Boolean.Parse((string)response["data"]["won"]);
+            if (!done)
+            {
+                form.EnableButtons();
+                form.AddMessageToConsole("Your turn...");
+            }
+            else
+            {
+                form.AddMessageToConsole("You lost!");
+            }
+        }
+
+        private void OpponentTurn(JObject response)
+        {
+            form.SetMark((string)response["mark"]);
+            form.AddMessageToConsole("Opponents turn...");
+            form.DisableButtons();
+        }
+
+        private void MyTurn(JObject response)
+        {
+            form.SetMark((string)response["mark"]);
+            form.AddMessageToConsole("Your turn...");
+            form.EnableButtons();
+        }
+
+        public JObject ReadMessage()
+        {
+            StringBuilder message = new StringBuilder();
+            try
+            {
+                int numberOfBytesRead = 0;
+                byte[] messageBytes = new byte[4];
+                stream.Read(messageBytes, 0, messageBytes.Length);
+                byte[] receiveBuffer = new byte[BitConverter.ToInt32(messageBytes, 0)];
+
+                do
+                {
+                    numberOfBytesRead = stream.Read(receiveBuffer, 0, receiveBuffer.Length);
+
+                    message.AppendFormat("{0}", Encoding.ASCII.GetString(receiveBuffer, 0, numberOfBytesRead));
+
+                }
+                while (message.Length < receiveBuffer.Length);
+            }catch(Exception e) { }
+
+            string response = message.ToString();
+            if (response.Equals(""))
+            {
+                dynamic winningMessage = new
+                {
+                    id = "won"
+                };
+                return JObject.Parse(JsonConvert.SerializeObject(winningMessage));
+            }
+            return JObject.Parse(response);
+        }
+
+        public void SendMessage(dynamic message)
+        {
+            string json = JsonConvert.SerializeObject(message);
+
+            byte[] prefixArray = BitConverter.GetBytes(json.Length);
+            byte[] requestArray = Encoding.Default.GetBytes(json);
+
+            byte[] buffer = new Byte[prefixArray.Length + json.Length];
+            prefixArray.CopyTo(buffer, 0);
+            requestArray.CopyTo(buffer, prefixArray.Length);
+            stream.Write(buffer, 0, buffer.Length);
+
+            form.DisableButtons();
+        }
+
+        internal void SetWon()
+        {
+            done = true;
+        }
+
+        internal void Close()
+        {
+            done = true;
+            stream.Close();
+        }
+    }
+}
